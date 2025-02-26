929edf61f9f46a764cdbf0c9bfcda158cf7151a4
Antar <antar@Antars-Laptop.local>
2025-02-26 21:24:23 +0300

    Modified files

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import Navbar from '@/components/Navbar';
 import Hero from '@/components/Hero';
 import Features from '@/components/Features';
 import Footer from '@/components/Footer';
+import VideoDropzone from "@/components/VideoDropzone";
 
 export default function Home() {
     return (
