16d88af7a13a3aa7968882669e7411ecb713bfbd
Antar <antar@Antars-Laptop.local>
2025-03-25 14:48:03 +0300

    Modified file

--- backend/server.js
+++ backend/server.js
@@ -149,13 +149,7 @@ app.use((req, res, next) => {
 });
 
 // Serve temporary files (they'll be cleaned up later)
-app.use('/temp', express.static(tempDir, {
-    setHeaders: function (res) {
-        res.set('Access-Control-Allow-Origin', 'https://www.sub0-translate.com'); // Change from '*' to specific domain
-        res.set('Access-Control-Allow-Methods', 'GET, OPTIONS');
-        res.set('Access-Control-Allow-Headers', 'Content-Type');
-    }
-}));
+app.use('/temp', express.static(tempDir));
 
 // Add a root endpoint for testing
 app.get('/', (req, res) => {
